6aa2adca7a126009bfa8d0e63940babed89008f2
Shaun Pedicini <scpedicini@gmail.com>
2025-07-07 16:48:56 -0500

    fix: format import statement for consistency in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import {WxtVitest} from "wxt/testing";
+import { WxtVitest } from "wxt/testing";
 
 export default defineConfig({
     plugins: [WxtVitest()],
